5faf8a0cb26694462f4ce6fa6a94a73c9bf26606
Dobolino <alex95d@gmail.com>
2026-09-17 08:13:15 +0200

    Vorhandene Fotos mit Original neu beschriften und Exportfehler beheben (#1)
    * Alte Fotos mit Original neu rendern und Exportfehler beheben
    
    * Nur eigene Offline-Caches entfernen
    
    * Grenzen der Foto-Migration dokumentieren

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="rundgang-v10";
+const CACHE="rundgang-v11";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",e=>{
@@ -6,7 +6,7 @@ self.addEventListener("install",e=>{
 });
 self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("rundgang-") && k!==CACHE).map(k=>caches.delete(k))))
       .then(()=>self.clients.claim())
   );
 });
